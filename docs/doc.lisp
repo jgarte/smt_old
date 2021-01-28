2537a69d2a870f396ba7679e6858a817c37941de
Amir <ateymuri63@gmail.com>
2021-01-28 19:13:01 +0100

    ..

--- docs/doc.lisp
+++ docs/doc.lisp
@@ -1,5 +1,4 @@
 (in-package #:smt-docs)
-(in-package :cl-markup)
 
 (sb-impl::defconstant-eqx +day-names+ 
     '("Monday" "Tuesday" "Wednesday"
@@ -12,31 +11,26 @@
 (with-open-file (s "/home/amir/Work/Lisp/smt/docs/index.html"
 		   :direction :output :if-does-not-exist :create
 		   :if-exists :supersede)
-  (format s (html :lang "eng"
-		  (:head (:meta :charset "UTF-8")	     
-			 (:title "SMT")
-			 (:style :type "text/css"
-				 "footer{ 
-            position: fixed; 
-            padding: 10px 10px 0px 10px; 
-            bottom: 0; 
-            width: 100%; 
-            /* Height of the Footer*/  
-            height: 40px; 
-            background: #acadac; 
-        }"))
-		  (:body
-		   (:header (:h1 "Symbolic Music Typesetting"))
-		   (:section "Hello MArkup")
-		   (:footer (format nil "Last modified: ~A" (multiple-value-bind (second minute hour date month year day-of-week dst-p tz)
-								(get-decoded-time)
-							      (format nil "~2,'0d:~2,'0d:~2,'0d of ~a, ~d/~2,'0d/~d (GMT~@d)"
-								      hour
-								      minute
-								      second
-								      (nth day-of-week +day-names+)
-								      month
-								      date
-								      year
-								      (- tz)))))
-		   ))))
+  (format s (cl-markup:html :lang "eng"
+			    (:head (:meta :charset "UTF-8")	     
+				   (:title "SMT")
+				   (:style :type "text/css"
+					   "html,body { background-color:#3df3;
+          padding: 0; margin: 0; } footer { position: fixed; padding:
+          10px 10px 0px 10px; bottom: 0; width: 100%; height: 40px;
+          background: #acadac; }"))
+			    (:body
+			     (:header (:h1 "Symbolic Music Typesetting"))
+			     (:section (documentation 'smt-engine::children 'function))
+			     (:footer (format nil "Last modified: ~A" (multiple-value-bind (second minute hour date month year day-of-week dst-p tz)
+									  (get-decoded-time)
+									(format nil "~2,'0d:~2,'0d:~2,'0d of ~a, ~d/~2,'0d/~d (GMT~@d)"
+										hour
+										minute
+										second
+										(nth day-of-week +day-names+)
+										month
+										date
+										year
+										(- tz)))))
+			     ))))
